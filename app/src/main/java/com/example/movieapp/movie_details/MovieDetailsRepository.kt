5e4f1d579d24a7065fc9b1c61dbf9a49c513b288
Gaurav Mehta <gmmmehta1111@gmail.com>
2020-07-07 19:24:11 +0530

    Added repository and viewmodel class for movie details.

---
+++ app/src/main/java/com/example/movieapp/movie_details/MovieDetailsRepository.kt
@@ -0,0 +1,24 @@
+package com.example.movieapp.movie_details
+
+import androidx.lifecycle.LiveData
+import com.example.movieapp.data.api.TheMovieDBInterface
+import com.example.movieapp.data.repository.MovieDetailsNetworkDataSource
+import com.example.movieapp.data.repository.NetworkState
+import com.example.movieapp.data.vo.MovieDetails
+import io.reactivex.disposables.CompositeDisposable
+
+class MovieDetailsRepository(private val apiService: TheMovieDBInterface) {
+
+    lateinit var movieDetailsNetworkDataSource: MovieDetailsNetworkDataSource
+
+    fun fetchSingleMovieDetails(compositeDisposable: CompositeDisposable, movieId: Int): LiveData<MovieDetails>{
+        movieDetailsNetworkDataSource = MovieDetailsNetworkDataSource(apiService, compositeDisposable)
+        movieDetailsNetworkDataSource.fetchMovieDetails(movieId)
+
+        return movieDetailsNetworkDataSource.downloadedMovieresponse
+    }
+
+    fun getMovieDetailsNetworkState(): LiveData<NetworkState>{
+        return movieDetailsNetworkDataSource.networkstate
+    }
+}
