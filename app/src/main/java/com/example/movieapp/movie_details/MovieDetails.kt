ec0d7024d44ea25636ab80ba45758f1e3576da92
Gaurav Mehta <gmmmehta1111@gmail.com>
2020-07-07 17:27:17 +0530

    Added data class for movie details.

---
+++ app/src/main/java/com/example/movieapp/movie_details/MovieDetails.kt
@@ -0,0 +1,12 @@
+package com.example.movieapp.movie_details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.movieapp.R
+
+class MovieDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+    }
+}
