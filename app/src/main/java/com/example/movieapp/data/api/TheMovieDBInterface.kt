69eb51470137005bb9895cc51ebb09f5db121d73
Gaurav Mehta <gmmmehta1111@gmail.com>
2020-07-07 17:54:53 +0530

    Added Api client interface.

--- app/src/main/java/com/example/movieapp/data/api/TheMovieDBInterface.kt
+++ app/src/main/java/com/example/movieapp/data/api/TheMovieDBInterface.kt
@@ -1,4 +1,13 @@
 package com.example.movieapp.data.api
 
+import com.example.movieapp.data.vo.MovieDetails
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface TheMovieDBInterface {
+
+    @GET("movie/{movie_id}")
+    fun getMovieDetails(@Path("movie_id") id: Int): Single<MovieDetails>
+
 }
