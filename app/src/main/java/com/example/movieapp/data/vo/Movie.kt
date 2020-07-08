fa52114c20b9e2bebf319e228df8c406c344b86c
Gaurav Mehta <gmmmehta1111@gmail.com>
2020-07-08 19:23:20 +0530

    Added movie list adapter.

--- app/src/main/java/com/example/movieapp/data/vo/Movie.kt
+++ app/src/main/java/com/example/movieapp/data/vo/Movie.kt
@@ -7,6 +7,7 @@ data class Movie(
 
     @SerializedName("id")
     val id: Int,
+    val title: String,
     @SerializedName("poster_path")
     val posterPath: String,
     @SerializedName("release_date")
