9350c75ec1607012a73c98afb4a87df8ec29f876
Gaurav Mehta <gmmmehta1111@gmail.com>
2020-07-08 20:21:54 +0530

    Displaying movie list data.

--- app/src/main/java/com/example/movieapp/data/api/TheMovieDBClient.kt
+++ app/src/main/java/com/example/movieapp/data/api/TheMovieDBClient.kt
@@ -14,7 +14,7 @@ const val BASE_URL = "https://api.themoviedb.org/3/"
 
 const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w342"
 
-const val FIRST_PAGE  = 1
+const val FIRST_PAGE  = 500
 const val POST_PER_PAGE = 20
 
 object TheMovieDBClient {
